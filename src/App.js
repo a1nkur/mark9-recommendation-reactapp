63e84e5fca7d0e012ccc341cce1ab2df4f9120c1
ankur29mac <ankur29mac@gmail.com>
2021-08-13 14:59:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { Switch, Route } from "react-router-dom";
+import style from "styled-components";
+
+function App() {
+  return (
+    <div className="App">
+      <Switch>
+        <Route path="/"></Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
+
+/* ---------------------------- STYLED COMPONENTS --------------------------- */
